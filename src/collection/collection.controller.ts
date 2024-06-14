30b1f42ec47f949b7966ec0c9d4961fbd30be54b
Artem-Elyashevich <elyashevich.artem06@gmail.com>
2024-06-14 20:21:29 +0300

    feat: collection service

--- src/collection/collection.controller.ts
+++ src/collection/collection.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CollectionService } from './collection.service';
 import { AuthGuard } from '@nestjs/passport';
 import { CollectionDto } from './dto/collection.dto';
@@ -18,5 +18,19 @@ export class CollectionController {
   @UsePipes(new ValidationPipe())
   public async create(@Body() collectionDto: CollectionDto) {
     return this.collectionService.create(collectionDto)
+  }
+
+  @Put("/:id")
+  @UseGuards(AuthGuard())
+  @UsePipes(new ValidationPipe())
+  public async update(@Param("id") id: string, @Body() data: CollectionDto) {
+    return this.collectionService.update(id, data)
+  }
+
+  @Delete("/:id")
+  @UseGuards(AuthGuard())
+  @UsePipes(new ValidationPipe())
+  public async remove(@Param("id") id: string) {
+    return this.collectionService.delete(id)
   }
 }
--- src/collection/collection.service.ts
+++ src/collection/collection.service.ts
@@ -1,8 +1,9 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Collection } from './collection.schema';
 import { CollectionDto } from './dto/collection.dto';
+import { NotFoundError } from 'rxjs';
 
 @Injectable()
 export class CollectionService {
@@ -23,5 +24,21 @@ export class CollectionService {
             console.log(err);
             throw new InternalServerErrorException()
         }
+    }
+
+    public async update(id: string, data: CollectionDto): Promise<Collection> {
+        const updatedProduct = await this.collectionRepository.findOneAndUpdate({ _id: id }, data, { new: true })
+        if (!updatedProduct) {
+            throw new InternalServerErrorException("Failed to update collection")
+        }
+        return updatedProduct
+    }
+
+    public async delete(name: string): Promise<void> {
+        const collection = await this.collectionRepository.findOne({name: name})
+        if (!collection) {
+            throw new NotFoundException("No such collection")
+        }
+        await this.collectionRepository.deleteOne({id: collection._id})
     }
 }
